1cf0c8a969acb52d94c0a1d0cd8e086559c24955
Александр <@DESKTOP-DSLL2UN>
2023-10-15 21:23:26 +0500

    Добавьте файлы проекта.

---
+++ Задание 3/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Задание_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        // регулярное выражение для адреса электронной почты
+        string pattern1 = "^(([a-zA-Z0-9_-]+\\.)*[a-zA-Z0-9_-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z]{2,6})?$";
+        // регулярное выражение для номера телефона
+        string pattern2 = "^([7]{1}[9]{1}[0-9]{9})?$";
+        // регулярное выражение для имени
+        string pattern3 = "^([A-ZА-Я]{1}[a-zа-я]+)?$";
+
+       
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bool i = true;
+            Person person = new Person();
+            while (i)
+            {
+                string str = inputBox.Text;
+
+                string[] substring = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (substring.Length == 3)
+                {
+                    foreach (string sub in substring)
+                    {
+
+                        Match match1 = Regex.Match(sub, pattern1);
+                        if (match1.Success)
+                        {
+                            person.setMail(match1.Value);
+                        }
+
+                        Match match2 = Regex.Match(sub, pattern2);
+                        if (match2.Success)
+                        {
+                            person.setPhone(match2.Value);
+                        }
+
+                        Match match3 = Regex.Match(sub, pattern3);
+                        if (match3.Success)
+                        {
+                            person.setName(match3.Value);
+                        }
+
+                    }
+                }
+                if (person.getName() != null && person.getPhone() != null && person.getMail() != null)
+                {
+                    i = false;
+                    outBox1.Text = person.getName();
+                    outBox2.Text = person.getPhone();
+                    outBox3.Text = person.getMail();
+                    outBox_4.Text = person.getId();
+                    break;
+                }
+                else
+                {
+                    inputBox.Text = "Вы неправильно ввели данные, повторите ввод. ";
+                    break;
+                }
+
+
+
+            }
+        }
+
+        private void clearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            inputBox.Clear();   
+        }
+    }
+}
---
+++ Задание 3/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_3
+{
+    internal class Person
+    {
+        private string name { get; set; }
+        private string phone { get; set; }
+        private string mail { get; set; }
+        static int id = 0;
+
+        //private bool n = false;
+        //private bool p = false;
+        //private bool m = false;
+
+        public Person(string name, string phone, string mail)
+        {
+            this.name = name;
+            this.phone = phone;
+            this.mail = mail;
+   //         id++;
+        }
+        public Person()
+        {
+            id++;
+        }
+
+        public void setName(string value)
+        {
+            this.name = value;
+        }
+
+        public void setPhone(string value)
+        {
+            this.phone = value;
+        }
+
+        public void setMail(string value)
+        {
+            this.mail = value;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getPhone()
+        {
+            return phone;
+        }
+
+
+        public string getMail()
+        {
+            return mail;
+        }
+        public string getId()
+        {
+            return id.ToString();
+        }
+
+
+    }
+}
